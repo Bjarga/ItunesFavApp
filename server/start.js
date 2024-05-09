2512202e34853b1102616a9ffdeed821b5a78309
Bjarga <85278388+Bjarga@users.noreply.github.com>
2024-05-09 21:08:15 +0200

    Delete server directory

--- server/start.js
+++
@@ -1,6 +0,0 @@
-const app = require("./server"); // Import the app from server.js
-
-const PORT = process.env.PORT || 3001;
-
-// Start the server
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
